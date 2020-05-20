489a9da352a809787d381af586eee75fd5626539
aratama <16192627+aratama@users.noreply.github.com>
2020-05-20 23:37:00 +0900

    add Unknown Type error

--- src/AutoEncoder/Encoder.elm
+++ src/AutoEncoder/Encoder.elm
@@ -5,8 +5,8 @@ import AutoEncoder.Type exposing (..)
 import Result
 
 
-encoderFromTypeName : List String -> Result Error String
-encoderFromTypeName name =
+encoderFromTypeName : Module -> List String -> Result Error String
+encoderFromTypeName mod name =
     case name of
         [ "Int" ] ->
             Ok "Json.Encode.int"
@@ -21,30 +21,35 @@ encoderFromTypeName name =
             Ok "Json.Encode.bool"
 
         [ "Dict", "String", contntType ] ->
-            encoderFromTypeName [ contntType ] |> Result.map (\t -> "Json.Encode.dict identity " ++ t)
+            encoderFromTypeName mod [ contntType ] |> Result.map (\t -> "Json.Encode.dict identity " ++ t)
 
         [ "List", contntType ] ->
-            encoderFromTypeName [ contntType ] |> Result.map (\t -> "Json.Encode.list " ++ t)
+            encoderFromTypeName mod [ contntType ] |> Result.map (\t -> "Json.Encode.list " ++ t)
 
         [ "Maybe", contntType ] ->
-            encoderFromTypeName [ contntType ] |> Result.map (\t -> "(Maybe.withDefault Json.Encode.null << Maybe.map " ++ t ++ ")")
+            encoderFromTypeName mod [ contntType ] |> Result.map (\t -> "(Maybe.withDefault Json.Encode.null << Maybe.map " ++ t ++ ")")
 
         [ typeName ] ->
-            Ok <| "encode" ++ typeName
+            case List.filter (\member -> member.name == String.join " " name) mod.members of
+                [] ->
+                    Err <| [ "Unknown Data Type: `" ++ String.join " " name ++ "`" ]
+
+                _ ->
+                    Ok <| "encode" ++ typeName
 
         _ ->
             Err <| [ "Unsupported Data Type: `" ++ String.join " " name ++ "`" ]
 
 
-generateEncoderFromTypeAlias : TypeAlias -> Result Error String
-generateEncoderFromTypeAlias alias =
+generateEncoderFromTypeAlias : Module -> TypeAlias -> Result Error String
+generateEncoderFromTypeAlias mod alias =
     let
         encoderName =
             "encode" ++ alias.name
 
         field : NameAndType -> Result Error String
         field entry =
-            encoderFromTypeName entry.typeName
+            encoderFromTypeName mod entry.typeName
                 |> Result.map
                     (\t ->
                         "(\"" ++ entry.name ++ "\", " ++ t ++ " value." ++ entry.name ++ ")"
@@ -92,7 +97,7 @@ generateEncoder : Module -> Result Error String
 generateEncoder mod =
     let
         results =
-            List.map generateEncoderFromTypeAlias mod.members
+            List.map (generateEncoderFromTypeAlias mod) mod.members
 
         members =
             List.filterMap Result.toMaybe results
