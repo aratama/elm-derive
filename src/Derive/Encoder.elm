79fc4ad4ed29e8b9bea202ca85f102386a56b116
aratama <16192627+aratama@users.noreply.github.com>
2020-06-02 00:00:10 +0900

    implement Encoder with elm-syntax

--- src/Derive/Encoder.elm
+++ src/Derive/Encoder.elm
@@ -3,14 +3,21 @@ module Derive.Encoder exposing (generateEncoder)
 import Derive.Type exposing (Module, ModuleMember(..), NameAndType, Type(..), TypeAliasDef)
 import Derive.Util exposing (Error, alphabets, asList, concatResults, derivedModuleName, indent, node, nodeValue, unlines)
 import Elm.Syntax.Declaration exposing (Declaration(..))
-import Elm.Syntax.Expression exposing (Expression(..), Function, FunctionImplementation)
+import Elm.Syntax.Expression exposing (Case, Expression(..), Function, FunctionImplementation)
 import Elm.Syntax.File exposing (File)
 import Elm.Syntax.Module exposing (Module)
+import Elm.Syntax.ModuleName exposing (ModuleName)
 import Elm.Syntax.Node exposing (..)
 import Elm.Syntax.Pattern exposing (Pattern(..))
-import Elm.Syntax.Type exposing (Type)
-import Elm.Syntax.TypeAnnotation exposing (TypeAnnotation(..))
+import Elm.Syntax.Signature exposing (Signature)
+import Elm.Syntax.Type exposing (Type, ValueConstructor)
+import Elm.Syntax.TypeAnnotation exposing (RecordField, TypeAnnotation(..))
 import Result
+
+
+functionAnnotation : ( ModuleName, String ) -> ( ModuleName, String ) -> TypeAnnotation
+functionAnnotation from to =
+    FunctionTypeAnnotation (node <| Typed (node from) []) (node <| Typed (node to) [])
 
 
 generateEncoder : File -> Result Error (List Declaration)
@@ -27,80 +34,141 @@ generateEncoderFromDeclaration file declaration =
                 |> Result.map
                     (\encoder ->
                         let
+                            typeName : String
+                            typeName =
+                                nodeValue aliasDecl.name
+
+                            name =
+                                "encode" ++ typeName
+
                             functionImplementation : FunctionImplementation
                             functionImplementation =
-                                { name = node <| "encode" ++ nodeValue aliasDecl.name
-                                , arguments = [ node <| VarPattern "value" ]
+                                { name = node <| name
+                                , arguments = []
                                 , expression = node encoder
+                                }
+
+                            signature : Signature
+                            signature =
+                                { name = node name
+                                , typeAnnotation = node <| functionAnnotation ( [], typeName ) ( [ "Json", "Encode" ], "Value" )
                                 }
 
                             function : Function
                             function =
                                 { documentation = Nothing
-                                , signature = Nothing
+                                , signature = Just <| node signature
                                 , declaration = node functionImplementation
                                 }
                         in
-                        [ FunctionDeclaration function
-                        ]
+                        [ FunctionDeclaration function ]
                     )
 
         CustomTypeDeclaration customTypeDecl ->
-            -- let
-            --     typeName =
-            --         nodeValue <| customTypeDecl.name
-            --     encoderName : String
-            --     encoderName =
-            --         "encode" ++ typeName
-            --     variants =
-            --         List.map
-            --             (\constructorNode ->
-            --                 let
-            --                     constructor =
-            --                         nodeValue constructorNode
-            --                 in
-            --                 if List.isEmpty constructor.arguments then
-            --                     constructor.name ++ " -> Json.Encode.string " ++ "\"" ++ constructor.name ++ "\""
-            --                 else
-            --                     let
-            --                         abc =
-            --                             alphabets (List.length constructor.arguments)
-            --                         vars =
-            --                             abc
-            --                                 |> List.intersperse ' '
-            --                                 |> String.fromList
-            --                         fields =
-            --                             List.indexedMap
-            --                                 (\i field ->
-            --                                     let
-            --                                         c =
-            --                                             Char.fromCode (97 + i)
-            --                                         encoder : String
-            --                                         encoder =
-            --                                             case generateEncoderFromTypeAnnotation file field of
-            --                                                 Err err ->
-            --                                                     "<ERROR>"
-            --                                                 Ok str ->
-            --                                                     str
-            --                                     in
-            --                                     "(\"" ++ String.fromChar c ++ "\", " ++ encoder ++ " " ++ String.fromChar c ++ ")"
-            --                                 )
-            --                                 constructor.arguments
-            --                                 |> (::) ("(\"tag\", Json.Encode.string \"" ++ constructor.name ++ "\")")
-            --                                 |> asList
-            --                     in
-            --                     constructor.name ++ " " ++ vars ++ " -> Json.Encode.object\n" ++ indent fields
-            --             )
-            --             customTypeDecl.constructors
-            -- in
-            -- Ok <|
-            --     unlines
-            --         [ encoderName ++ " : " ++ typeName ++ " -> Json.Encode.Value"
-            --         , encoderName ++ " variant"
-            --         , indent "= case value of"
-            --         , indent <| indent <| unlines variants
-            --         ]
-            Ok []
+            let
+                typeName : String
+                typeName =
+                    nodeValue <| customTypeDecl.name
+
+                encoderName : String
+                encoderName =
+                    "encode" ++ typeName
+
+                variants : List (Result Error Case)
+                variants =
+                    List.map
+                        (\constructorNode ->
+                            let
+                                constructor : ValueConstructor
+                                constructor =
+                                    nodeValue constructorNode
+
+                                abc : List String
+                                abc =
+                                    alphabets (List.length constructor.arguments)
+                                        |> List.map String.fromChar
+
+                                first : Expression
+                                first =
+                                    TupledExpression [ node <| Literal "tag", node <| Literal (nodeValue constructor.name) ]
+
+                                fields : Result Error (List Expression)
+                                fields =
+                                    constructor.arguments
+                                        |> List.indexedMap (\i fieldNode -> ( i, fieldNode ))
+                                        |> concatResults
+                                            (\( i, fieldNode ) ->
+                                                let
+                                                    field : TypeAnnotation
+                                                    field =
+                                                        nodeValue fieldNode
+                                                in
+                                                generateEncoderFromTypeAnnotation file field
+                                                    |> Result.map
+                                                        (\encoder ->
+                                                            let
+                                                                c : String
+                                                                c =
+                                                                    String.fromChar (Char.fromCode (97 + i))
+                                                            in
+                                                            TupledExpression
+                                                                [ node <| Literal c
+                                                                , node <| Application [ node encoder, node <| FunctionOrValue [] c ]
+                                                                ]
+                                                        )
+                                            )
+                                        |> Result.map ((::) first)
+
+                                constructorCase : Result Error Case
+                                constructorCase =
+                                    fields
+                                        |> Result.map
+                                            (\fs ->
+                                                let
+                                                    xs : List (Node Expression)
+                                                    xs =
+                                                        List.map node fs
+
+                                                    h : Node Expression
+                                                    h =
+                                                        node (FunctionOrValue [ "Json", "Encode" ] "object")
+                                                in
+                                                ( node <| NamedPattern { moduleName = [], name = nodeValue constructor.name } (List.map (\c -> node <| VarPattern c) abc)
+                                                , node <| Application [ h, node <| ListExpr xs ]
+                                                )
+                                            )
+                            in
+                            constructorCase
+                        )
+                        customTypeDecl.constructors
+            in
+            concatResults identity variants
+                |> Result.map
+                    (\cases ->
+                        let
+                            signature : Signature
+                            signature =
+                                { name = node encoderName
+                                , typeAnnotation = node <| functionAnnotation ( [], typeName ) ( [ "Json", "Encode" ], "Value" )
+                                }
+                        in
+                        [ FunctionDeclaration
+                            { documentation = Nothing
+                            , signature = Just <| node signature
+                            , declaration =
+                                node <|
+                                    { name = node <| encoderName
+                                    , arguments = [ node <| VarPattern "value" ]
+                                    , expression =
+                                        node <|
+                                            CaseExpression
+                                                { expression = node <| FunctionOrValue [] "value"
+                                                , cases = cases
+                                                }
+                                    }
+                            }
+                        ]
+                    )
 
         _ ->
             Ok []
@@ -159,18 +227,42 @@ generateEncoderFromTypeAnnotation file typeAnnotation =
         --             Err <| [ "Unknown Data Type: `" ++ typeName ++ "`" ]
         --         _ ->
         --             Ok <| FunctionOrValue (derivedModuleName file) ("encode" ++ typeName)
-        -- RecordType record ->
-        --     let
-        --         field : NameAndType -> Result Error String
-        --         field entry =
-        --             generateEncoderFromTypeAnnotation file entry.typeName
-        --                 |> Result.map
-        --                     (\k ->
-        --                         "(\"" ++ entry.name ++ "\", " ++ k ++ " value." ++ entry.name ++ ")"
-        --                     )
-        --     in
-        --     concatResults field record
-        --         |> Result.map (\fields -> "(\\value -> Json.Encode.object \n" ++ (indent <| indent <| asList fields) ++ ")")
+        Record record ->
+            let
+                field : Node RecordField -> Result Error Expression
+                field recordFieldNode =
+                    let
+                        -- recordField : RecordField
+                        ( Node _ name, Node _ fieldTypeAnnotation ) =
+                            nodeValue recordFieldNode
+                    in
+                    generateEncoderFromTypeAnnotation file fieldTypeAnnotation
+                        |> Result.map
+                            (\k ->
+                                TupledExpression
+                                    [ node <| Literal name
+                                    , node <|
+                                        Application
+                                            [ node k
+                                            , node <| RecordAccess (node <| FunctionOrValue [] "value") (node name)
+                                            ]
+                                    ]
+                            )
+            in
+            concatResults field record
+                |> Result.map
+                    (\fields ->
+                        LambdaExpression
+                            { args = [ node <| VarPattern "value" ]
+                            , expression =
+                                node <|
+                                    Application
+                                        [ node <| FunctionOrValue [ "Json", "Encode" ] "object"
+                                        , node <| ListExpr <| List.map node fields
+                                        ]
+                            }
+                    )
+
         _ ->
             Err <| [ "Unsupported Data Type at generateEncoderFromType" ]
 
