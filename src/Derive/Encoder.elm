0f4a67516d97ec1b53c4eba23ea4bfe4992c65bc
aratama <16192627+aratama@users.noreply.github.com>
2020-06-02 00:04:52 +0900

    update

--- src/Derive/Encoder.elm
+++ src/Derive/Encoder.elm
@@ -1,16 +1,15 @@
 module Derive.Encoder exposing (generateEncoder)
 
-import Derive.Type exposing (Module, ModuleMember(..), NameAndType, Type(..), TypeAliasDef)
-import Derive.Util exposing (Error, alphabets, asList, concatResults, derivedModuleName, indent, node, nodeValue, unlines)
+import Derive.Type exposing (ModuleMember(..), Type(..))
+import Derive.Util exposing (Error, alphabets, concatResults, node, nodeValue)
 import Elm.Syntax.Declaration exposing (Declaration(..))
 import Elm.Syntax.Expression exposing (Case, Expression(..), Function, FunctionImplementation)
 import Elm.Syntax.File exposing (File)
-import Elm.Syntax.Module exposing (Module)
 import Elm.Syntax.ModuleName exposing (ModuleName)
 import Elm.Syntax.Node exposing (..)
 import Elm.Syntax.Pattern exposing (Pattern(..))
 import Elm.Syntax.Signature exposing (Signature)
-import Elm.Syntax.Type exposing (Type, ValueConstructor)
+import Elm.Syntax.Type exposing (ValueConstructor)
 import Elm.Syntax.TypeAnnotation exposing (RecordField, TypeAnnotation(..))
 import Result
 
@@ -204,35 +203,40 @@ generateEncoderFromTypeAnnotation file typeAnnotation =
                 |> Result.map
                     (\encoder -> Application [ node <| FunctionOrValue [ "Json", "Encode" ] "maybe", node encoder ])
 
-        -- TypeRef typeName [] ->
-        --     let
-        --         filtered =
-        --             List.filter
-        --                 (\declarationNode ->
-        --                     let
-        --                         declaration : Declaration
-        --                         declaration =
-        --                             nodeValue declarationNode
-        --                     in
-        --                     case declaration of
-        --                         AliasDeclaration ta ->
-        --                             nodeValue ta.name == typeName
-        --                         CustomTypeDeclaration tm ->
-        --                             nodeValue tm.name == typeName
-        --                 )
-        --                 file.declarations
-        --     in
-        --     case filtered of
-        --         [] ->
-        --             Err <| [ "Unknown Data Type: `" ++ typeName ++ "`" ]
-        --         _ ->
-        --             Ok <| FunctionOrValue (derivedModuleName file) ("encode" ++ typeName)
+        Typed (Node _ ( [], moduleMemberTypeName )) [] ->
+            let
+                filtered =
+                    List.filter
+                        (\declarationNode ->
+                            let
+                                declaration : Declaration
+                                declaration =
+                                    nodeValue declarationNode
+                            in
+                            case declaration of
+                                AliasDeclaration ta ->
+                                    nodeValue ta.name == moduleMemberTypeName
+
+                                CustomTypeDeclaration tm ->
+                                    nodeValue tm.name == moduleMemberTypeName
+
+                                _ ->
+                                    False
+                        )
+                        file.declarations
+            in
+            case filtered of
+                [] ->
+                    Err <| [ "Unknown Data Type: `" ++ moduleMemberTypeName ++ "`" ]
+
+                _ ->
+                    Ok <| FunctionOrValue [] ("encode" ++ moduleMemberTypeName)
+
         Record record ->
             let
                 field : Node RecordField -> Result Error Expression
                 field recordFieldNode =
                     let
-                        -- recordField : RecordField
                         ( Node _ name, Node _ fieldTypeAnnotation ) =
                             nodeValue recordFieldNode
                     in
@@ -265,137 +269,3 @@ generateEncoderFromTypeAnnotation file typeAnnotation =
 
         _ ->
             Err <| [ "Unsupported Data Type at generateEncoderFromType" ]
-
-
-
--- generateEncoderFromType : Module -> Type -> Result Error String
--- generateEncoderFromType mod target =
---     case target of
---         TypeRef "Int" [] ->
---             Ok "Json.Encode.int"
---         TypeRef "String" [] ->
---             Ok "Json.Encode.string"
---         TypeRef "Float" [] ->
---             Ok "Json.Encode.float"
---         TypeRef "Bool" [] ->
---             Ok "Json.Encode.bool"
---         TypeRef "Dict" [ TypeRef "String" [], content ] ->
---             generateEncoderFromType mod content |> Result.map (\t -> "(Json.Encode.dict identity " ++ t ++ ")")
---         TypeRef "List" [ contntType ] ->
---             generateEncoderFromType mod contntType |> Result.map (\t -> "(Json.Encode.list " ++ t ++ ")")
---         TypeRef "Maybe" [ contntType ] ->
---             generateEncoderFromType mod contntType |> Result.map (\t -> "(Maybe.withDefault Json.Encode.null << Maybe.map " ++ t ++ ")")
---         TypeRef typeName [] ->
---             let
---                 filtered =
---                     List.filter
---                         (\member ->
---                             case member of
---                                 TypeAliasMember ta ->
---                                     ta.name == typeName
---                                 TypeMember tm ->
---                                     tm.name == typeName
---                         )
---                         mod.members
---             in
---             case filtered of
---                 [] ->
---                     Err <| [ "Unknown Data Type: `" ++ typeName ++ "`" ]
---                 _ ->
---                     Ok <| "encode" ++ typeName
---         RecordType record ->
---             let
---                 field : NameAndType -> Result Error String
---                 field entry =
---                     generateEncoderFromType mod entry.typeName
---                         |> Result.map
---                             (\k ->
---                                 "(\"" ++ entry.name ++ "\", " ++ k ++ " value." ++ entry.name ++ ")"
---                             )
---             in
---             concatResults field record
---                 |> Result.map (\fields -> "(\\value -> Json.Encode.object \n" ++ (indent <| indent <| asList fields) ++ ")")
---         _ ->
---             Err <| [ "Unsupported Data Type at generateEncoderFromType" ]
--- generateEncoderFromModuleMember : Module -> ModuleMember -> Result Error String
--- generateEncoderFromModuleMember mod member =
---     case member of
---         TypeAliasMember typeAlias ->
---             generateEncoderFromTypeAlias mod typeAlias
---         TypeMember typeMember ->
---             let
---                 encoderName =
---                     "encode" ++ typeMember.name
---                 variants =
---                     List.map
---                         (\variant ->
---                             if List.isEmpty variant.fields then
---                                 variant.name ++ " -> Json.Encode.string " ++ "\"" ++ variant.name ++ "\""
---                             else
---                                 let
---                                     abc =
---                                         alphabets (List.length variant.fields)
---                                     vars =
---                                         abc
---                                             |> List.intersperse ' '
---                                             |> String.fromList
---                                     fields =
---                                         List.indexedMap
---                                             (\i field ->
---                                                 let
---                                                     c =
---                                                         Char.fromCode (97 + i)
---                                                     encoder : String
---                                                     encoder =
---                                                         case generateEncoderFromType mod field of
---                                                             Err err ->
---                                                                 "<ERROR>"
---                                                             Ok str ->
---                                                                 str
---                                                 in
---                                                 "(\"" ++ String.fromChar c ++ "\", " ++ encoder ++ " " ++ String.fromChar c ++ ")"
---                                             )
---                                             variant.fields
---                                             |> (::) ("(\"tag\", Json.Encode.string \"" ++ variant.name ++ "\")")
---                                             |> asList
---                                 in
---                                 variant.name ++ " " ++ vars ++ " -> Json.Encode.object\n" ++ indent fields
---                         )
---                         typeMember.variants
---             in
---             Ok <|
---                 unlines
---                     [ encoderName ++ " : " ++ typeMember.name ++ " -> Json.Encode.Value"
---                     , encoderName ++ " variant"
---                     , indent "= case value of"
---                     , indent <| indent <| unlines variants
---                     ]
--- generateEncoderFromTypeAlias : Module -> TypeAliasDef -> Result Error String
--- generateEncoderFromTypeAlias mod alias =
---     let
---         encoderName =
---             "encode" ++ alias.name
---         result : Result Error String
---         result =
---             generateEncoderFromType mod alias.body
---     in
---     case result of
---         Err errors ->
---             Err errors
---         Ok str ->
---             Ok <|
---                 unlines
---                     [ encoderName ++ " : " ++ alias.name ++ " -> Json.Encode.Value"
---                     , encoderName ++ "\n" ++ indent "= " ++ str
---                     ]
--- generateEncoder : Module -> Result Error String
--- generateEncoder mod =
---     concatResults (generateEncoderFromModuleMember mod) mod.members
---         |> Result.map
---             (\members ->
---                 unlines
---                     [ "-- encoders -------------------------------------------------------------"
---                     , ""
---                     , String.join "\n\n" members
---                     ]
---             )
